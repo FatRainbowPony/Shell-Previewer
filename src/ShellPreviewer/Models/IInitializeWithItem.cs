7d368d1f77b581a1811873fd2f88de80b2f0d817
FatRainbowPony <semfady@yandex.ru>
2024-10-07 11:07:59 +0700

    Added IInitializeWithItem.cs

---
+++ src/ShellPreviewer/Models/IInitializeWithItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ShellPreviewer.Models
+{
+    [ComImport]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    [Guid("7F73BE3F-FB79-493C-A6C7-7EE14E245841")]
+    public interface IInitializeWithItem
+    {
+        #region Methods
+
+        #region Public
+        public void Initialize(IShellItem psi, uint grfMode);
+        #endregion Public
+
+        #endregion Methods
+    }
+}
